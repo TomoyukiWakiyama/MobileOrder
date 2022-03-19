99dd33f847c4578740fa63ba30a12ddb819a533e
TomoyukiWakiyama <kreva2002@gmail.com>
2022-03-19 12:00:32 +0900

    Category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('categories')->insert([
+            [
+                'name' => 'お弁当',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'おかずのみ',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'サイドメニュー',
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'イベント用',
+                'created_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
